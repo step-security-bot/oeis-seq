efe0643b91c98a06d3cbad097ce6c948bdfbd575
Reid Hochstedler <reidhoch@users.noreply.github.com>
2021-06-03 16:42:00 -0400

    Fix 3.8 (#6)
    * Python 3.8 does not have math.lcm
    
    * LCM is simple enough to not worry about specific Python versions

--- tests/utils/test_coprime.py
+++ tests/utils/test_coprime.py
@@ -1,12 +1,16 @@
 # coding=utf-8
+from math import gcd
+
 from hypothesis import given
 from hypothesis.strategies import integers
-
 from oeis.utils import coprime
-from math import lcm
 
 
-@given(a=integers(min_value=2, max_value=100), b=integers(min_value=2, max_value=100))
+def lcm(x: int, y: int) -> int:
+    return (x * y) // gcd(x, y)
+
+
+@given(a=integers(), b=integers())
 def test_lcm_property(a: int, b: int) -> None:
     if coprime(a, b):
         assert lcm(a, b) == a * b
