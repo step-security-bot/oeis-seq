05ff6927bf7a3785655a123ca9daddf7dff26ada
Reid Hochstedler <reidhoch@gmail.com>
2021-07-07 08:23:20 -0400

    Version bump.

--- oeis/__init__.py
+++ oeis/__init__.py
@@ -45,7 +45,7 @@ from .vauban import vauban
 from .weird import weird
 from .woodall import woodall
 
-__version__ = "0.1.4"
+__version__ = "0.1.5"
 __all__ = [
     "abundant",
     "buttered_croissant",
