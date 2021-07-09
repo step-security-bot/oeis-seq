223f298778e4e6847b82dce4748100c28245b581
Reid Hochstedler <reidhoch@gmail.com>
2021-07-09 14:29:31 -0400

    Check for duplicate keys.

--- tests/registry/test_registry.py
+++ tests/registry/test_registry.py
@@ -2,6 +2,8 @@
 # flake8: noqa
 from typing import Callable, Iterable
 
+import pytest
+
 from oeis.registry import OEISRegistry
 
 
@@ -17,3 +19,19 @@ def test_register() -> None:
     expected = one
     actual = registry["A000012"]
     assert expected is actual
+
+
+def test_duplicate_entries_throw_KeyError() -> None:
+    registry: OEISRegistry[Callable[[], Iterable[int]]] = OEISRegistry[
+        Callable[[], Iterable[int]]
+    ]()
+
+    @registry.register(identifier="A000012")
+    def one() -> Iterable[int]:
+        yield 1
+
+    with pytest.raises(KeyError):
+
+        @registry.register(identifier="A000012")
+        def two() -> Iterable[int]:
+            yield 1
